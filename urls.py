dc552fb314c5710bb93b66be9cfff2f79a5b58a0
Pavel Katsev <pkatsev@gmail.com>
2010-10-02 16:09:52 +0400

    added a template for the experimental youtube player

--- urls.py
+++ urls.py
@@ -4,4 +4,7 @@ urlpatterns = patterns('',
     (r'^logout/$', 'django.contrib.auth.views.logout'),
     (r'^$', 'facebook.views.login'),
     (r'^youtubes/$', 'facebook.views.youtube_vids'),
+
+    # for andrey
+    (r'^player/$', 'django.views.generic.simple.direct_to_template', {'template': 'ytplayer.html'}),
 )
