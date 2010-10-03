6f8e6bb5da9191496c632387371858a403d372c4
Pavel Katsev <pkatsev@gmail.com>
2010-10-03 04:06:03 +0400

    first stab at the player + minor login improvements

--- facebook/views.py
+++ facebook/views.py
@@ -11,49 +11,6 @@ from django.contrib.auth.decorators import login_required
 
 import facebook_sdk as fb
 
-def login(request):
-    error = None
-    vid_list = ''
-
-    fb_cookie = fb.get_user_from_cookie(
-        request.COOKIES, settings.FACEBOOK_APP_ID, settings.FACEBOOK_APP_SECRET)
-    if fb_cookie:
-        access_token = fb_cookie['access_token']
-        user = auth.authenticate(access_token=access_token)
-
-        # haxor into here
-        graph = fb.GraphAPI(access_token)
-        # import ipdb; ipdb.set_trace()
-        home_feed = graph.get_connections('me', 'home', type='video', limit=100)
-        vids = (post for post in home_feed['data'] if post['type'] == 'video')
-        html_unit = """<div>
-    <label>From: %(from)s</label>
-    <object width="480" height="385">
-        <param name="movie" value="%(link)s"></param>
-        <param name="allowFullScreen" value="true"></param>
-        <param name="allowscriptaccess" value="always"></param>
-        <embed src="%(link)s" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="385"></embed>
-    </object>
-    <p>date: %(created_time)s</p>
-</div>"""
-        vid_list = "<br/>".join(
-            [html_unit % vid for vid in vids]
-        )
-
-        if user:
-            if user.is_active:
-                auth.login(request, user)
-                # return HttpResponseRedirect('/yay/')
-            else:
-                error = 'AUTH_DISABLED'
-        else:
-            error = 'AUTH_FAILED'
-    elif 'error_reason' in request.GET:
-        error = 'AUTH_DENIED'
-
-    template_context = {'settings': settings, 'error': error, 'vids': vid_list}
-    return render_to_response('login.html', template_context, context_instance=RequestContext(request))
-
 # Find a JSON parser
 try:
     import json
@@ -67,6 +24,42 @@ except ImportError:
         from django.utils import simplejson
         _write_json = lambda s: simplejson.dumps(s)
 
+def login(request):
+    error = None
+    vid_list = ''
+
+    fb_cookie = fb.get_user_from_cookie(
+        request.COOKIES, settings.FACEBOOK_APP_ID, settings.FACEBOOK_APP_SECRET)
+    if fb_cookie:
+        access_token = fb_cookie['access_token']
+        user = auth.authenticate(access_token=access_token)
+
+        # haxor into here
+        # graph = fb.GraphAPI(access_token)
+        # # import ipdb; ipdb.set_trace()
+        # home_feed = graph.get_connections('me', 'home', type='video', limit=100)
+        # vids = (post for post in home_feed['data'] if post['type'] == 'video')
+        # html_unit = "<div><p>From: %(from)s</p><p>Link: %(link)s</p><p>Date: %(created_time)s</p></div>"
+        # vid_list = "<br/>".join(
+        #     [html_unit % vid for vid in vids]
+        # )
+
+        if user:
+            if user.is_active:
+                auth.login(request, user)
+                # return HttpResponseRedirect('/')
+            else:
+                error = 'AUTH_DISABLED'
+        else:
+            error = 'AUTH_FAILED'
+    elif 'error_reason' in request.GET:
+        error = 'AUTH_DENIED'
+    else:
+        auth.logout(request)
+
+    template_context = {'settings': settings, 'error': error, 'vids': vid_list}
+    return render_to_response('login.html', template_context, context_instance=RequestContext(request))
+
 @login_required
 def youtube_vids(request):
     fb_cookie = fb.get_user_from_cookie(
@@ -76,7 +69,24 @@ def youtube_vids(request):
         user = auth.authenticate(access_token=access_token)
 
         graph = fb.GraphAPI(access_token)
-        home_feed = graph.get_connections('me', 'home', type='video', limit=100)
+        home_feed = graph.get_connections('me', 'home', type='video', limit=50)
         vids = (post for post in home_feed['data'] if post['type'] == 'video' and 'youtube.com' in post['link'])
         return HttpResponse(_write_json(list(vids)))
+
     return HttpResponseServerError()
+
+
+def youtube_vids_stub(request):
+    vids = [
+        {
+            'link': 'http://www.youtube.com/watch?v=V3GmNMbuMbc',
+            'from': {'name': 'Christian Bale'},
+            'created_time': '00:00 1969'
+        },
+        {
+            'link': 'http://www.youtube.com/watch?v=pX5gRaxcviE',
+            'from': {'name': 'Jimi Hendrix'},
+            'created_time': '00:00 2010'
+        }
+    ]
+    return HttpResponse(_write_json(vids))
--- urls.py
+++ urls.py
@@ -1,10 +1,15 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^logout/$', 'django.contrib.auth.views.logout'),
     (r'^$', 'facebook.views.login'),
     (r'^youtubes/$', 'facebook.views.youtube_vids'),
+    (r'^youtubezzz/$', 'facebook.views.youtube_vids_stub'),
 
     # for andrey
     (r'^player/$', 'django.views.generic.simple.direct_to_template', {'template': 'ytplayer.html'}),
+    
+    # media
+    (r'^statics/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
